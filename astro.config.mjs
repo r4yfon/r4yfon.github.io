2f49d35ab5286eb96d7cc6151bace20baea7338d
rayfon <75479544+r4yfon@users.noreply.github.com>
2025-06-23 20:03:43 +0300

    typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://r4yfon.github.io","
+  site: "https://r4yfon.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
